a5d8597a0cfd8447a0a9ab3326343485a540db4e
dalius.zukauskas <dalius.zukauskas@elgama.eu>
2023-03-05 07:47:07 +0200

    init

---
+++ DockerHoneypot/Helpers/TarForDockerFileDir.cs
@@ -0,0 +1,53 @@
+﻿using ICSharpCode.SharpZipLib.Tar;
+using System.Formats.Tar;
+
+namespace DockerHoneypot.Helpers
+{
+    internal static class TarForDockerFileDir
+    {
+        [Obsolete]
+        internal static Stream CreateTarballForDockerfileDirectory(string directory)
+        {
+            var tarball = new MemoryStream();
+            var files = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories);
+
+            using var archive = new TarOutputStream(tarball)
+            {
+                //Prevent the TarOutputStream from closing the underlying memory stream when done
+                IsStreamOwner = false
+            };
+
+            foreach (var file in files)
+            {
+                //Replacing slashes as KyleGobel suggested and removing leading /
+                string tarName = file[directory.Length..].Replace('\\', '/').TrimStart('/');
+
+                //Let's create the entry header
+                var entry = ICSharpCode.SharpZipLib.Tar.TarEntry.CreateTarEntry(tarName);
+                using var fileStream = File.OpenRead(file);
+                entry.Size = fileStream.Length;
+                entry.TarHeader.Mode = Convert.ToInt32("100755", 8);
+                archive.PutNextEntry(entry);
+
+                //Now write the bytes of data
+                byte[] localBuffer = new byte[32 * 1024];
+                while (true)
+                {
+                    int numRead = fileStream.Read(localBuffer, 0, localBuffer.Length);
+                    if (numRead <= 0)
+                        break;
+
+                    archive.Write(localBuffer, 0, numRead);
+                }
+
+                //Nothing more to do with this entry
+                archive.CloseEntry();
+            }
+            archive.Close();
+
+            //Reset the stream and return it, so it can be used by the caller
+            tarball.Position = 0;
+            return tarball;
+        }
+    }
+}
---
+++ DockerHoneypot/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Docker.DotNet;
+using Docker.DotNet.Models;
+using DockerHoneypot.Helpers;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+var progress = new Progress<JSONMessage>();
+var captureTarStream = TarForDockerFileDir.CreateTarballForDockerfileDirectory("../../../Images/capture");
+var captureImageBuildParameters = new ImageBuildParameters() { Tags = new List<string> { "capture" }, Dockerfile = "../../../Images/capture/Dockerfile" };
+
+var dockerClient = new DockerClientConfiguration().CreateClient();
+//RESET
+var containers = await dockerClient.Containers.ListContainersAsync(new ContainersListParameters() { All = true });
+foreach (var container in containers)
+{
+    if(container.Status == "Created")
+    {
+        Console.WriteLine($"Removing container: {container.Image}");
+        await dockerClient.Containers.RemoveContainerAsync(container.ID, new ContainerRemoveParameters());
+    }
+    else if(container.State == "running" || container.State == "exited")
+    {
+        Console.WriteLine($"Stopping container: {container.Image}");
+        await dockerClient.Containers.StopContainerAsync(container.ID, new ContainerStopParameters() { WaitBeforeKillSeconds = 10 });
+        Console.WriteLine($"Removing container: {container.Image}");
+        await dockerClient.Containers.RemoveContainerAsync(container.ID, new ContainerRemoveParameters());
+    }
+}
+
+
+//var captureStream = File.OpenRead(pathToCaptureTar);
+Console.WriteLine($"Building image...");
+await dockerClient.Images.BuildImageFromDockerfileAsync(captureImageBuildParameters, captureTarStream, null, null, progress);
+
+using var saveCaptureImageStream = await dockerClient.Images.SaveImageAsync("capture");
+Console.WriteLine($"Creating container...");
+var createNetworkContainerResponse = await dockerClient.Containers.CreateContainerAsync(new CreateContainerParameters() { Name = "whaler_capture", Image = "capture", Entrypoint = new List<string>() { "/bin/sh", "/app/wrapper.sh" } });
+Console.WriteLine($"Starting container...");
+await dockerClient.Containers.StartContainerAsync(createNetworkContainerResponse.ID, new ContainerStartParameters());
+Console.WriteLine($"Container started.");
+
+//var reportingImageBuildParameters = new ImageBuildParameters() { Tags = new List<string> { "reporting" } };
+////dockerClient.Images.BuildImageFromDockerfileAsync();
+//using var reportingTarball = TarForDockerFileDir.CreateTarballForDockerfileDirectory("../../../Images/reporting");
+//using var reportingResponseStream = await dockerClient.Images.BuildImageFromDockerfileAsync(reportingTarball, reportingImageBuildParameters);
+
+//using var saveReportingImageStream = await dockerClient.Images.SaveImageAsync("reporting");
